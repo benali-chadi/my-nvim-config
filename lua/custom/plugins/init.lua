4bf3c8493e53b8838c8a696088a6c3cc06d2c8d8
Benalioulhaj-Chadi_bcgprod <benalioulhaj.chadi@bcg.com>
2024-09-02 11:27:20 +0100

    pomo plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -264,5 +264,33 @@ return {
 			suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
 			-- log_level = 'debug',
 		}
+	},
+
+	-- Pomodoro
+	{
+		"epwalsh/pomo.nvim",
+		version = "*", -- Recommended, use latest release instead of latest commit
+		lazy = true,
+		cmd = { "TimerStart", "TimerRepeat", "TimerSession" },
+		dependencies = {
+			-- Optional, but highly recommended if you want to use the "Default" timer
+			"rcarriga/nvim-notify",
+		},
+		opts = {
+			sessions = {
+				reading = {
+					{ name = "Focus", duration = "25m" },
+					{ name = "Break", duration = "5m" },
+					{ name = "Focus", duration = "25m" },
+				},
+				work = {
+					{ name = "Focus", duration = "25m" },
+					{ name = "Break", duration = "5m" },
+					{ name = "Focus", duration = "25m" },
+					{ name = "Break", duration = "5m" },
+					{ name = "Focus", duration = "25m" },
+				}
+			}
+		},
 	}
 }
