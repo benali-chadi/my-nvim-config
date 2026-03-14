9cfa5e22c3193aea50909ab9f894501ac11fca6a
chadi <benalioulhajchadi@gmail.com>
2026-03-14 04:11:21 +0000

    lazy docker

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -351,7 +351,7 @@ return {
 						-- name = 'llama',
 						schema = {
 							model = {
-								default = "deepseek-r1",
+								default = "lama",
 							},
 							-- num_ctx = {
 							-- 	default = 20000,
@@ -380,5 +380,27 @@ return {
 				},
 			},
 		}
+	},
+	-- Lazy Docker
+	{
+		"mgierada/lazydocker.nvim",
+		dependencies = { "akinsho/toggleterm.nvim" },
+		config = function()
+			require("lazydocker").setup({
+				border = "curved", -- valid options are "single" | "double" | "shadow" | "curved"
+				width = 0.9, -- width of the floating window (0-1 for percentage, >1 for absolute columns)
+				height = 0.9, -- height of the floating window (0-1 for percentage, >1 for absolute rows)
+			})
+		end,
+		event = "BufRead",
+		keys = {
+			{
+				"<leader>ld",
+				function()
+					require("lazydocker").open()
+				end,
+				desc = "Open Lazydocker floating window",
+			},
+		},
 	},
 }
